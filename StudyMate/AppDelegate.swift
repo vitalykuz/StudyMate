d53bd13d1ebf8cc1df00937d87de8aae8c4a4a8c
Vitaly Kuzenkov <vitalykuzenkov@gmail.com>
2017-05-08 14:07:24 +1000

    Installed FBSDKCore and Login pods. Fixed error with other pods. Ready to implement the Facebook login

--- StudyMate/AppDelegate.swift
+++ StudyMate/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Firebase
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
 		
+		// Enables Firabase
 		FIRApp.configure()
 		
 		return true
