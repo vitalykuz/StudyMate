c5fdf22c82b2208307215181b685f9838b7cf5d7
Vitaly Kuzenkov <vitalykuzenkov@gmail.com>
2017-05-08 16:06:30 +1000

    Email registration and auth is completed

--- StudyMate/SignInVC.swift
+++ StudyMate/SignInVC.swift
@@ -11,23 +11,28 @@ import FBSDKLoginKit
 import FBSDKCoreKit
 import Firebase
 
-class SignInVC: UIViewController {
+class SignInVC: UIViewController, UITextFieldDelegate {
 
 	@IBOutlet var emailTextField: TextFieldCustomView!
 	@IBOutlet var passwordTextField: TextFieldCustomView!
 	
-	
-	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
+		emailTextField.delegate = self
+		passwordTextField.delegate = self
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 	}
-
 	
-	/* 
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		self.view.endEditing(true)
+		return false
+	}
+	
+	/*
 		@brief This methods tries to authenticate a user from Facebook.
 		 If successful, than it sends the FB credentials to Firebase
 	*/
@@ -61,17 +66,29 @@ class SignInVC: UIViewController {
 		})
 	}
 	
+	
+	/*
+	@brief This methods tries to sign in a Firebase user with email. 
+	 If the user does not exist (does not have an acc), it creates a new user with provided email and password.
+	*/
 	@IBAction func signInButtonTapped(_ sender: Any) {
-		
-		
-		
-		
+		//checks if text fields are not empty
+		if let email = emailTextField.text, let password = passwordTextField.text {
+			FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+				if ( error == nil ) {
+					print("Vitaly: success login with email Firebase")
+				} else {
+					FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error ) in
+						if ( error != nil ) {
+							print("Vitaly: unable to auth with email firebase")
+						} else {
+							print("Vitaly: new user with email created with Firebase")
+						}
+					})
+				}
+			})
+		}
 	}
-	
-	
-	
-	
-	
 }
 
 
