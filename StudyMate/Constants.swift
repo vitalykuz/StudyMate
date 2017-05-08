bf313e98234d4e9319c55f2d9f727db49edb522b
Vitaly Kuzenkov <vitalykuzenkov@gmail.com>
2017-05-08 12:19:01 +1000

    Custom View for buttons and  text fields implemented

---
+++ StudyMate/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  StudyMate
+//
+//  Created by Vitaly Kuzenkov on 8/5/17.
+//  Copyright © 2017 Vitaly Kuzenkov. All rights reserved.
+//
+
+import UIKit
+
+//defines colors for shadow
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
