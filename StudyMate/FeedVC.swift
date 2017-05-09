41852fcc68ffb3f8cdff338086bd06470da0e57a
Vitaly Kuzenkov <vitalykuzenkov@gmail.com>
2017-05-09 19:21:58 +1000

    Feed UI improvements

--- StudyMate/FeedVC.swift
+++ StudyMate/FeedVC.swift
@@ -10,13 +10,29 @@ import UIKit
 import SwiftKeychainWrapper
 import Firebase
 
-class FeedVC: UIViewController {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+	@IBOutlet var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+		tableView.delegate = self
+		tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
+	
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return tableView.dequeueReusableCell(withIdentifier: "postCell") as! PostTableViewCell
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 3
+	}
+	
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
---
+++ StudyMate/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  StudyMate
+//
+//  Created by Vitaly Kuzenkov on 9/5/17.
+//  Copyright © 2017 Vitaly Kuzenkov. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
