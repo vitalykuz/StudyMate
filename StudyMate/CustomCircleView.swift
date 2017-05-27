46c2daacd3db54891f7cbc190455f42802da7898
Vitaly Kuzenkov <vitalykuzenkov@gmail.com>
2017-05-27 20:19:35 +1000

    Added UIImagePickerController and methods required to get it working

--- StudyMate/CustomCircleView.swift
+++ StudyMate/CustomCircleView.swift
@@ -12,16 +12,11 @@ class CustomCircleView: UIImageView {
 
 	override func awakeFromNib() {
 		super.awakeFromNib()
-		
-		//defines the parameters for shadows
-		layer.shadowColor = UIColor(displayP3Red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 1.0).cgColor
-		layer.shadowOpacity = 1.0
-		layer.shadowRadius = 5.0
-		layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
 	}
 	
 	override func layoutSubviews() {
 		super.layoutSubviews()		
 		layer.cornerRadius = self.frame.width / 2
+		clipsToBounds = true
 	}
 }
