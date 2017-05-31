9b17c8821f77995bb3e8a5abb234314818c84b21
Vitaly Kuzenkov <vitalykuzenkov@gmail.com>
2017-05-31 14:31:37 +1000

    I can now create a post and update the firebase

--- StudyMate/PostVC.swift
+++ StudyMate/PostVC.swift
@@ -7,17 +7,32 @@
 //
 
 import UIKit
+import Firebase
 
-class PostVC: UIViewController {
+class PostVC: UIViewController, UITextViewDelegate, UITextFieldDelegate {
 	@IBOutlet var subjectNameField: TextFieldCustomView!
 	@IBOutlet var locationField: TextFieldCustomView!
 	@IBOutlet var whenLabel: TextFieldCustomView!
 	@IBOutlet var postDescription: UITextView!
 	
+	private var userName: String?
+	private var uni: String?
+	private var profileImageUrl: String?
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 
+		postDescription.delegate = self
+		subjectNameField.delegate = self
+		whenLabel.delegate = self
+		locationField.delegate = self
         // Do any additional setup after loading the view.
+		
+		postDescription.text = PROVIDE_POST_DESCRIPTION
+		postDescription.textColor = UIColor.lightGray
+		
+		self.findUser()
+		
     }
 	
 	@IBAction func createPostButtonTapped(_ sender: Any) {
@@ -36,11 +51,15 @@ class PostVC: UIViewController {
 			return
 		}
 		
-//		guard let description = postDescription.text, description != "" else {
-//			postDescription.attributedPlaceholder = NSAttributedString(string: ERROR_POST_DESCRIPTION_EMPTY, attributes: [NSForegroundColorAttributeName: UIColor.red])
-//			return
-//		}
+		guard let description = postDescription.text, description != "" else {
+			postDescription.text = ERROR_POST_DESCRIPTION_EMPTY
+			postDescription.textColor = UIColor.red
+			return
+		}
 		
+		self.postToFirebase()
+	}
+	
 //		if let imgData = UIImageJPEGRepresentation(img, 0.2) {
 //			
 //			let imgUid = NSUUID().uuidString
@@ -59,24 +78,67 @@ class PostVC: UIViewController {
 //				}
 //			}
 //		}
+
+	
+	func postToFirebase() {
+		let post: Dictionary<String, Any> = [
+			SUBJECT_NAME: self.subjectNameField.text as Any,
+			LOCATION: self.locationField.text as Any,
+			MEETING_TIME: self.whenLabel.text as Any,
+			POST_DESCRIPTION: self.postDescription.text as Any,
+			PROFILE_IMAGE_URL: self.profileImageUrl as Any,
+			USER_NAME: self.userName as Any,
+			UNIVERSITY: self.uni ?? ERROR_DEFAULT_VALUE,
+			COMMENTS: 0 as Any,
+			LIKES: 0 as Any
+		]
+		
+		let firebasePost = DataService.ds.REF_POSTS.childByAutoId()
+		firebasePost.setValue(post)
+	}
+
+	func findUser() {
+		let ref = DataService.ds.REF_BASE
+		let userID = FIRAuth.auth()?.currentUser?.uid
+		_ = ref.child(USERS).child(userID!).observeSingleEvent(of: .value, with: { (snapshot) in
+			if let dictionary = snapshot.value as? [String: AnyObject] {
+				self.userName = dictionary[USER_NAME] as? String
+				self.uni = dictionary[UNIVERSITY] as? String
+				self.profileImageUrl = dictionary[PROFILE_IMAGE_URL] as? String
+				print("Current user name: \(String(describing: self.userName))")
+				print("Uni: \(String(describing: self.uni))")
+				print("Image url: \(String(describing: self.profileImageUrl))")
+			}
+		})
 	}
 	
-//	func postToFirebase(imgUrl: String) {
-//		let post: Dictionary<String, AnyObject> = [
-//			"caption": captionField.text! as AnyObject,
-//			"imageUrl": imgUrl as AnyObject,
-//			"likes": 0 as AnyObject
-//		]
-//		
-//		let firebasePost = DataService.ds.REF_POSTS.childByAutoId()
-//		firebasePost.setValue(post)
-//		
-//		captionField.text = ""
-//		imageSelected = false
-//		imageAdd.image = UIImage(named: "add-image")
-//		
-//		tableView.reloadData()
-//	}
-
-
+	
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		self.view.endEditing(true)
+		return false
+	}
+	
+	func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+		if(text == "\n") {
+			textView.resignFirstResponder()
+			return false
+		}
+		return true
+	}
+	
+	func textViewDidBeginEditing(_ textView: UITextView) {
+		if textView.textColor == UIColor.lightGray || textView.textColor == UIColor.red {
+			textView.text = nil
+			textView.textColor = UIColor.black
+		}
+	}
+	
+	func textViewDidEndEditing(_ textView: UITextView) {
+		if textView.text.isEmpty {
+			textView.text = PROVIDE_POST_DESCRIPTION
+			textView.textColor = UIColor.lightGray
+		}
+	}
+	
+	
 }
